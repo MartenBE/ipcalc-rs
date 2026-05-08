f20d45f13221253e8df1c3bc3e75d8080dad01c6
Martijn Saelens <martijn.saelens@protonmail.com>
2026-05-08 18:15:49 +0200

    Checkpoint

--- src/main.rs
+++ src/main.rs
@@ -4,60 +4,65 @@ use std::net::Ipv4Addr;
 mod ipv4;
 
 #[derive(Parser)]
-#[clap(disable_help_flag = true)]
 struct Args {
-    ip_address: String,
+    #[arg(help = "print addresses in binary.", long, required = false, short)]
+    binary: bool,
 
-    #[arg(long)]
-    all_info: bool,
-
-    #[arg(help = "Show hostname determined via DNS.", long, short)]
-    hostname: bool,
+    #[arg(value_name = "PREFIX")]
+    ip: String,
 
     #[arg(
-        action = clap::ArgAction::HelpLong,
-        help = "Show this help message.",
+        help = "Split the provided network using the provided prefix/netmask.",
         long,
-        short = '?',
+        required = false,
+        short,
+        value_name = "PREFIX"
     )]
-    help: Option<bool>,
-
-    #[arg(
-        help = "Print information on the provided IP address (default).",
-        long,
-        short
-    )]
-    info: bool,
-
-    #[arg(help = "Print network in a the reverse DNS format.", long)]
-    reverse_dns: bool,
-    //
-    // #[arg(
-    //     help = "Split the provided network using the provided prefix/netmask.",
-    //     long,
-    //     required = false,
-    //     short = 'S',
-    //     value_name = "PREFIX"
-    // )]
-    // split: Option<String>,
+    split: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let ip_address = args.ip_address.parse::<Ipv4Addr>().unwrap_or_else(|_| {
-        eprintln!("Bad IPv4 address: {}", args.ip_address);
-        std::process::exit(1);
-    });
+    let ip_parts = args.ip.split_terminator('/').collect::<Vec<_>>();
+    let ip_address_str = ip_parts[0];
 
-    if args.all_info {
-        ipv4::print_all_info(ip_address.into());
-    } else if args.hostname {
-        ipv4::print_hostname(ip_address.into());
-    } else if args.reverse_dns {
-        ipv4::print_reverse_dns(ip_address.into());
+    let ip_address = parse_ipv4_address(ip_address_str);
+
+    ipv4::print_info(ip_address.into());
+
+    let format = if args.binary {
+        ipv4::Ipv4Format::Binary
     } else {
-        // args.info
-        ipv4::print_info(ip_address.into());
+        ipv4::Ipv4Format::Decimal
+    };
+
+    if ip_parts.len() == 2 {
+        println!();
+
+        let prefix_str = ip_parts[1];
+        let prefix = parse_prefix(prefix_str);
+
+        ipv4::print_subnet_info(ip_address.into(), prefix, format);
+
+        if let Some(split_prefix_str) = args.split {
+            println!();
+            let split_prefix = parse_prefix(&split_prefix_str);
+            ipv4::print_subnet_splits(ip_address.into(), prefix, split_prefix, format);
+        }
     }
+}
+
+fn parse_ipv4_address(ip_str: &str) -> Ipv4Addr {
+    ip_str.parse::<Ipv4Addr>().unwrap_or_else(|_| {
+        eprintln!("ERROR! Bad IPv4 address: {}", ip_str);
+        std::process::exit(1);
+    })
+}
+
+fn parse_prefix(prefix_str: &str) -> u8 {
+    prefix_str.parse::<u8>().unwrap_or_else(|_| {
+        eprintln!("ERROR! Bad prefix: {}", prefix_str);
+        std::process::exit(1);
+    })
 }
